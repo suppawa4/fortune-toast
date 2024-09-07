19ddf03a9ac994bda2cb559e121df0342d355aae
SUPPAWAT NAMKORN <64130500123@ad.sit.kmutt.ac.th>
2024-09-07 16:38:14 +0700

    fix NEXT_ENV for BASE_URL

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Loading from "@/app/components/loading";
+import axios from "axios";
 
 export default function Home() {
   const [card, setCard] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const BASE_API = process.env.NEXT_PUBLIC_LOCAL_URL;
 
   const initCard = async () => {
     const cachedCard = localStorage.getItem("tarotCard");
@@ -13,8 +15,8 @@ export default function Home() {
       setIsLoading(false);
     } else {
       try {
-        const response = await fetch("/api/tarot");
-        const newCard = await response.json();
+        const response = await axios.get(`${BASE_API}/api/tarot`);
+        const newCard = response.data;
 
         localStorage.setItem("tarotCard", JSON.stringify(newCard));
         setCard(newCard);
--- src/app/services/TarotService.js
+++ src/app/services/TarotService.js
@@ -13,37 +13,39 @@ const cardDesc = async (card) => {
   const response = await result.response;
   return response.text();
 };
-// const getTarot = async () => {
-//   let card = {};
-//   let allTarots = {};
-//   const initCard = async () => {
-//     const jsonPath = path.join(
-//       process.cwd(),
-//       "src",
-//       "app",
-//       "data",
-//       "tarot-images.json"
-//     );
-//     const fileContents = await fs.readFile(jsonPath, "utf8");
-//     allTarots = JSON.parse(fileContents);
-//     const cards = allTarots.cards;
-//     const randomIndex = Math.floor(Math.random() * cards.length);
-//     card = cards[randomIndex];
-//   };
-//   const getCardImg = (cardImg) => {
-//     return `/cards/${cardImg}`;
-//   };
 
-//   await initCard();
-//   const { name, number, arcana, suit, img } = card;
-//   const cardDescription = await cardDesc(name);
+const getCard = async () => {
+  let card = {};
+  let allTarots = {};
+  const initCard = async () => {
+    const jsonPath = path.join(
+      process.cwd(),
+      "src",
+      "app",
+      "data",
+      "tarot-images.json"
+    );
+    const fileContents = await fs.readFile(jsonPath, "utf8");
+    allTarots = JSON.parse(fileContents);
+    const cards = allTarots.cards;
+    const randomIndex = Math.floor(Math.random() * cards.length);
+    card = cards[randomIndex];
+  };
+  const getCardImg = (cardImg) => {
+    return `/cards/${cardImg}`;
+  };
 
-//   const responseData = {
-//     name: name,
-//     desc: cardDescription.replace(/\n/g, "").trim(),
-//     img: getCardImg(img),
-//   };
-//   return responseData;
-// };
+  await initCard();
+  const { name, number, arcana, suit, img } = card;
+  const cardDescription = await cardDesc(name);
 
-export { cardDesc };
+  const responseData = {
+    name: name,
+    desc: cardDescription.replace(/\n/g, "").trim(),
+    img: getCardImg(img),
+  };
+
+  return responseData;
+};
+
+export { cardDesc, getCard };
