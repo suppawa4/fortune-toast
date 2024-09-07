c59be883951fcd588bf736e1a9ac9659e2f01d89
SUPPAWAT NAMKORN <64130500123@ad.sit.kmutt.ac.th>
2024-09-07 16:09:55 +0700

    fixing CSS, loadingState

--- src/app/components/loading.js
+++ src/app/components/loading.js
@@ -1,8 +1,8 @@
-import Image from "next/image";
+'use client'
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <Image
+    <div className="flex justify-center items-center my-80">
+      <img
         src="/infinite-spinner.svg"
         width={150}
         height={150}
--- src/app/services/TarotService.js
+++ src/app/services/TarotService.js
@@ -13,37 +13,37 @@ const cardDesc = async (card) => {
   const response = await result.response;
   return response.text();
 };
-const getTarot = async () => {
-  let card = {};
-  let allTarots = {};
-  const initCard = async () => {
-    const jsonPath = path.join(
-      process.cwd(),
-      "src",
-      "app",
-      "data",
-      "tarot-images.json"
-    );
-    const fileContents = await fs.readFile(jsonPath, "utf8");
-    allTarots = JSON.parse(fileContents);
-    const cards = allTarots.cards;
-    const randomIndex = Math.floor(Math.random() * cards.length);
-    card = cards[randomIndex];
-  };
-  const getCardImg = (cardImg) => {
-    return `/cards/${cardImg}`;
-  };
+// const getTarot = async () => {
+//   let card = {};
+//   let allTarots = {};
+//   const initCard = async () => {
+//     const jsonPath = path.join(
+//       process.cwd(),
+//       "src",
+//       "app",
+//       "data",
+//       "tarot-images.json"
+//     );
+//     const fileContents = await fs.readFile(jsonPath, "utf8");
+//     allTarots = JSON.parse(fileContents);
+//     const cards = allTarots.cards;
+//     const randomIndex = Math.floor(Math.random() * cards.length);
+//     card = cards[randomIndex];
+//   };
+//   const getCardImg = (cardImg) => {
+//     return `/cards/${cardImg}`;
+//   };
 
-  await initCard();
-  const { name, number, arcana, suit, img } = card;
-  const cardDescription = await cardDesc(name);
+//   await initCard();
+//   const { name, number, arcana, suit, img } = card;
+//   const cardDescription = await cardDesc(name);
 
-  const responseData = {
-    name: name,
-    desc: cardDescription.replace(/\n/g, "").trim(),
-    img: getCardImg(img),
-  };
-  return responseData;
-};
+//   const responseData = {
+//     name: name,
+//     desc: cardDescription.replace(/\n/g, "").trim(),
+//     img: getCardImg(img),
+//   };
+//   return responseData;
+// };
 
-export { cardDesc, getTarot };
+export { cardDesc };
